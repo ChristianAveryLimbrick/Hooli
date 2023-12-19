285c458252e859c4e17d5b1b629c71012afbfb42
Christian Limbrick <sixo@Christians-Air.attlocal.net>
2023-12-19 14:59:53 -0600

    Got more work done on the UI. Logo, Search , and Buttons now in place. Footer set up as well. Clear from here to proceed with building out the rest of the footer, adding navbar and button reactivity. Also switched to Bootstrap styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,50 +1,118 @@
+import React from "react";
 
+import HooliLogo from "../Hooli.png";
+import "flowbite";
+
+("use client");
 
 export default function Home() {
-    return (
-        <div className="header">
-            <Logo />
-            <Search />
-        </div>
-    );
+  return (
+    <div>
+      <div className="container">
+        <Logo />
+        <Search />
+        <Btn />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-const Logo = () => {    
-    return (
-        <div style={{
-            position: 'absolute',
-            top: '30%', // Adjust this value as needed
-            left: '50%',
-            transform: 'translateX(-50%)',
-            width: '100%' // Ensures text is centered within the full width
-        }}>
-            <img src="./Hooli.png" alt="" />
-        </div>
-    );
-}
+const Logo = () => {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      <img src={HooliLogo} alt="Hooli Logo" />
+    </div>
+  );
+};
+
+const searchStyles = {
+  width: "100%", // Use a percentage for responsiveness
+  maxWidth: "1000px", // Maximum width
+  height: "45px", // Moderate height
+  borderRadius: "24px", // Rounded border
+  borderColor: "#dfe1e5", // Light grey border color
+  borderWidth: "1px", // Thin border
+  padding: "10px 20px", // Padding inside the search bar
+  marginTop: "55px",
+  marginRight: "200px",
+};
 
 const Search = () => {
-    const searchStyles = {
-        width: '580px',
-        height: '40px',
-        borderRadius: '15px',
-        margin: 'auto',
-        position: 'absolute',
-        top: '50%', // Adjust this value as needed
-        left: '50%',
-        transform: 'translate(-50%, -50%)'
-    };
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          style={searchStyles}
+          className="px-8 py-2 focus:outline-none "
+          placeholder="Search..."
+        />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div style={{ height: '100vh' }}>
-            <div className="flex border-2 rounded">
-                <input 
-                    type="text" 
-                    style={searchStyles}
-                    className="px-4 py-2 focus:outline-none" 
-                    placeholder="Test..."
-                />
-            </div>
-        </div>
-    );
-}
+const Btn = () => {
+  const buttonStyle = {
+    backgroundColor: "#D3D3D3", // Grey background
+    color: "black", // Black text
+    padding: "8px 16px", // Vertical and horizontal padding
+    borderRadius: "12px", // Pill shape
+    border: "none", // No border
+    outline: "none", // No outline
+    cursor: "pointer", // Cursor changes to pointer on hover
+    margin: "5px", // Margin around buttons
+    marginTop: "25px",
+  };
+
+  return (
+    <div>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          console.log("Search clicked");
+        }}
+      >
+        Search
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          console.log("I'm feeling lucky clicked");
+        }}
+      >
+        I'm lucky
+      </button>
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>HooliMail</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+const Footer = () => {
+  return (
+      <footer className="bg-secondary text-white py-3">
+          <div className="container text-center">
+              <a href="#" className="text-white">
+                  Donate to fight climate change
+              </a>
+          </div>
+      </footer>
+  );
+};
+
